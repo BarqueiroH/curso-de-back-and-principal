a2cfb49e0347df8014252b566f0c087b161fa63f
barqueiroH <mateushbarqueiro26@gmail.com>
2024-05-17 09:57:07 -0300

    thuth false .js

---
+++ Javascript-Tipos-variaveis-e-funcoes-main/thuthfalsy.js
@@ -0,0 +1,25 @@
+//truth e false sao primitivos de comparação
+
+//1 e 0
+
+console.lof(0==false);
+console.lof(1==true);
+console.lof(0==true);
+console.log(1==false);
+
+console.log(""==false);
+
+let minhavar;
+
+// não recebe nada indefinido 
+
+let testenull = null;
+
+console.log(minhavar);
+console.log(testenull);
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeoff(minhavar));
+console.log(typeoff(testenull));
