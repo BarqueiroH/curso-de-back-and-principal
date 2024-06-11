7034842cf122fbbe2ba9d49c8b082cb6d5185846
BarqueiroH <163554550+BarqueiroH@users.noreply.github.com>
2024-06-11 09:56:41 -0300

    set Convercoes

---
+++ Javascript-Tipos-variaveis-e-funcoes-main/setconvercoes.js
@@ -0,0 +1,22 @@
+ //conversao implicita
+
+const numero = 465;
+const numeroString = "465";
+
+console.log(numero === numeroString);
+
+  //conversao inplicita
+   //acontece quando java script converte o numero em string
+   //e depois compara
+  //as duas strings
+
+console.log(numero == numeroString);
+console.log(numero + numeroString);
+
+//conversao explicita
+  // number
+let novoNumero = number(numeroString);
+console.log(numero + novoNumero);
+
+ //string
+
